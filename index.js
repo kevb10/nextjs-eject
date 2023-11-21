42fb13cdbdaf63a89c4fd8ccc1c030968048ca18
Kevin Manase <gasydev@gmail.com>
2023-11-21 14:45:17 -0600

    transform using ATS instead of regex

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
 const fs = require('fs');
 const path = require('path');
-const { handlerRegex } = require('./regexes');
+const { transformFile } = require('./transform');
 
+// Eject function to transform all route files
 function eject(projectDir) {
-    // Get api folder path
     const apiDir = path.join(projectDir, 'src', 'app', 'api');
-    // Rename api folder to lambdas
-    fs.renameSync(apiDir, path.join(projectDir, 'src', 'app', 'lambdas'));
+  const lambdasDir = path.join(projectDir, 'src', 'app', 'lambdas');
+  fs.renameSync(apiDir, lambdasDir);
 
-    // Loop through lambdas folder
-    fs.readdirSync(path.join(projectDir, 'src', 'app', 'lambdas')).forEach(dir => {
-        // Get route file path
-        const routePath = path.join(projectDir, 'src', 'app', 'lambdas', dir, 'route.ts');
-        // Rename route.ts to handler.ts  
+  fs.readdirSync(lambdasDir).forEach(dir => {
+    const routePath = path.join(lambdasDir, dir, 'route.ts');
         const handlerPath = routePath.replace('route.ts', 'handler.ts');
         fs.renameSync(routePath, handlerPath);
-        // Rewrite handler contents
-        const routeFileContents = fs.readFileSync(handlerPath, 'utf8');
-
-        const handlerContents = routeFileContents
-            .replace(handlerRegex, 'export async function handler(event, context)')
-            // Replace Next.js response pattern with Lambda response pattern
-            .replace(/return NextResponse.json\((.*), \{(.*)\}\);/g, (_, body, headers) => {
-                // Capture status code if available
-                const statusMatch = /status: (\d+)/.exec(headers);
-                const statusCode = statusMatch ? statusMatch[1] : 200;
-                return `return convertNextResponseToLambdaResponse(${body}, ${statusCode}, {${headers}});`;
-            })
-            // Include the conversion function in each handler file
-            .concat('\n\n' + convertNextResponseToLambdaResponse.toString());
-
-        // Write new handler content  
-        fs.writeFileSync(handlerPath, handlerContents);
+    transformFile(handlerPath);
     });
 }
 
---
+++ transform.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const generate = require('@babel/generator').default;
+const t = require('@babel/types');
+
+// Function to transform a file using AST
+function transformFile(filePath) {
+	const code = fs.readFileSync(filePath, 'utf8');
+
+	// Parse the code into an AST
+	const ast = parser.parse(code, {
+		sourceType: 'module',
+		plugins: ['typescript']
+	});
+
+	// Traverse the AST to apply transformations
+	traverse(ast, {
+		FunctionDeclaration(path) {
+			// Replace specific function declarations
+			if (path.node.id && ['GET', 'POST', 'OPTION', 'PATCH', 'DELETE'].includes(path.node.id.name)) {
+				path.node.id.name = 'handler';
+				path.node.async = true; // Ensure the function is async
+				path.node.params = [t.identifier('event'), t.identifier('context')]; // Change parameters
+			}
+		},
+		ReturnStatement(path) {
+			// Logic to modify return statements for Lambda format
+			if (path.node.argument && path.node.argument.type === 'CallExpression' &&
+				path.node.argument.callee.property &&
+				path.node.argument.callee.property.name === 'json') {
+
+				let responseBody = path.node.argument.arguments[0];
+				let responseOptions = path.node.argument.arguments[1];
+
+				// Create the new return statement for Lambda
+				let newReturnStatement = t.returnStatement(
+					t.objectExpression([
+						t.objectProperty(t.identifier('statusCode'), responseOptions ?
+							t.memberExpression(responseOptions, t.identifier('status')) : t.numericLiteral(200)),
+						t.objectProperty(t.identifier('headers'), t.objectExpression([
+							t.objectProperty(t.stringLiteral('Content-Type'), t.stringLiteral('application/json'))
+						])),
+						t.objectProperty(t.identifier('body'), t.callExpression(
+							t.memberExpression(t.identifier('JSON'), t.identifier('stringify')), [responseBody]))
+					])
+				);
+
+				path.replaceWith(newReturnStatement);
+			}
+		}
+	});
+
+	// Generate the modified code
+	const output = generate(ast, {}, code);
+	fs.writeFileSync(filePath, output.code);
+}
+
+module.exports = { transformFile };
