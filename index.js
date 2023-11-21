3d58fc08935cd7eb5950483e9e44b31482e9e5d0
Kevin Manase <gasydev@gmail.com>
2023-11-21 16:25:48 -0600

    Refactor route file naming and directory structure

--- index.js
+++ index.js
@@ -2,18 +2,41 @@ const fs = require('fs');
 const path = require('path');
 const { transformFile } = require('./transform');
 
-// Eject function to transform all route files
-function eject(projectDir) {
-    const apiDir = path.join(projectDir, 'src', 'app', 'api');
-  const lambdasDir = path.join(projectDir, 'src', 'app', 'lambdas');
+function processDirectory(directory) {
+  fs.readdirSync(directory, { withFileTypes: true }).forEach(entry => {
+    const entryPath = path.join(directory, entry.name);
+    if (entry.isDirectory()) {
+      processDirectory(entryPath); // Recursively process subdirectories
+    } else if (entry.isFile() && entry.name === 'route.ts') {
+      const handlerPath = entryPath.replace('route.ts', 'handler.ts');
+      fs.renameSync(entryPath, handlerPath);
+      transformFile(handlerPath);
+    }
+  });
+}
+
+function eject(projectDir = process.cwd()) {
+  const possibleRoots = [
+    path.join(projectDir, 'src', 'app', 'api'),
+    path.join(projectDir, 'app', 'api')
+  ];
+  console.log(possibleRoots)
+  let apiDir;
+  for (const root of possibleRoots) {
+    if (fs.existsSync(root)) {
+      apiDir = root;
+      break;
+    }
+  }
+
+  if (!apiDir) {
+    throw new Error('Could not find api directory');
+  }
+
+  const lambdasDir = apiDir.replace(/api$/, 'lambdas');
   fs.renameSync(apiDir, lambdasDir);
 
-  fs.readdirSync(lambdasDir).forEach(dir => {
-    const routePath = path.join(lambdasDir, dir, 'route.ts');
-        const handlerPath = routePath.replace('route.ts', 'handler.ts');
-        fs.renameSync(routePath, handlerPath);
-    transformFile(handlerPath);
-    });
+  processDirectory(lambdasDir); // Process the directory and its subdirectories
 }
 
 module.exports = { eject };
